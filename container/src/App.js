cd62b55b72098b820932ec05fd1f21123b82fb90
hsimao <e087754958@gmail.com>
2021-01-23 16:38:33 +0800

    add: container conncet MarketingApp

--- container/src/App.js
+++ container/src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
-import { mount } from "marketing/MarketingApp";
+import MarketingApp from "./components/MarketingApp";
 
-console.log("mount", mount);
 export default () => {
-  return <h1>Hi there!</h1>;
+  return (
+    <div>
+      <h1>Container</h1>;
+      <hr />
+      <MarketingApp />
+    </div>
+  );
 };
