b171a607d8b0ee46bcb7326d61bddac6c6932c66
hsimao <e087754958@gmail.com>
2021-01-26 21:54:20 +0800

    add: 新增 marketing 與 container 雙向溝通 router history

--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -1,17 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createMemoryHistory, createBrowserHistory } from "history";
 import App from "./App";
 
 // 創建 mount function
-const mount = (el) => {
-  ReactDOM.render(<App />, el);
+const mount = (el, { onNavigate, defaultHistory }) => {
+  const history = defaultHistory || createMemoryHistory();
+
+  if (onNavigate) {
+    history.listen(onNavigate);
+  }
+
+  ReactDOM.render(<App history={history} />, el);
+
+  return {
+    onParentNavigate: ({ pathname: nextPathname }) => {
+      // 預防無限循環
+      const { pathname } = history.location;
+      if (pathname !== nextPathname) {
+        console.log("from  container", nextPathname);
+        history.push(nextPathname);
+      }
+    }
+  };
 };
 
 // 如果是 development 將直接調用 mount
 if (process.env.NODE_ENV === "development") {
   const devRoot = document.getElementById("_marketing-dev-root");
   if (devRoot) {
-    mount(devRoot);
+    mount(devRoot, { defaultHistory: createBrowserHistory() });
   }
 }
 
